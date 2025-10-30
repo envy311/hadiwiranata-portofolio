037af1013eb9834768a2ad1b00716121f76ef5c2
envy311 <hiroyukikei2@gmail.com>
2025-10-30 22:26:08 +0700

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,9 +1,3 @@
-import HeroImage from "/assets/hero-img.webp";
-
-const Image = {
-  HeroImage,
-};
-
 export default Image;
 
 import Tools1 from "/assets/tools/vscode.png";
@@ -175,4 +169,5 @@ export const listsertif = [
     dad: "200",
   },
  
+
 ];
